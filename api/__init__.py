f9c59808b87730a8c44c27d9eaae37f00ab0f5c9
Subham8989 <srigurujay1@gmail.com>
2025-06-19 10:55:49 +0530

    Initial Commit

---
+++ api/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_restful import Api
+
+
+from .main import User, UserList
+
+
+app = Flask(__name__)
+api = Api(app)
+
+api.add_resource(UserList, "/api/users", endpoint="userlist", strict_slashes=False)
+api.add_resource(User, "/api/users/<string:user_id>", endpoint="users", strict_slashes=False)
---
+++ api/main.py
@@ -0,0 +1,24 @@
+from flask_restful import Resource
+
+class UserList(Resource):
+  def get(self):
+    pass
+
+  def post(self):
+    pass
+
+
+class User(Resource):
+  def get(self, user_id):
+    pass
+
+  def put(self, user_id):
+    pass
+
+  def patch(self, user_id):
+    pass
+
+  def delete(self, user_id):
+    pass
+
+  
